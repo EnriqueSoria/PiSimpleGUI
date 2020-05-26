86204a79f7410c4de1228e36cca9bfb5219e2d4d
EnriqueSoria <ensomag@inf.upv.es>
2020-05-26 15:08:57 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import smbus2
+
+LM75_ADDRESS     = 0x48
+
+LM75_TEMP_REGISTER    = 0
+LM75_CONF_REGISTER    = 1
+LM75_THYST_REGISTER    = 2
+LM75_TOS_REGISTER    = 3
+
+LM75_CONF_SHUTDOWN     = 0
+LM75_CONF_OS_COMP_INT    = 1
+LM75_CONF_OS_POL    = 2
+LM75_CONF_OS_F_QUE    = 3
+
+
+class LM75(object):
+  def __init__(self, mode=LM75_CONF_OS_COMP_INT, address=LM75_ADDRESS, busnum=1):
+    self._mode = mode
+    self._address = address
+    self._bus = smbus2.SMBus(busnum)
+
+  def regdata2float (self, regdata):
+    return (regdata / 32.0) / 8.0
+  def toFah(self, temp):
+    return (temp * (9.0/5.0)) + 32.0
+  def to_celsius(self, temp):
+    f = self.toFah(temp)
+    return (f-32) * 5/9
+
+  def getTemp(self, celsius=True):
+    raw = self._bus.read_word_data(self._address, LM75_TEMP_REGISTER) & 0xFFFF
+    raw = ((raw << 8) & 0xFF00) + (raw >> 8)
+    return self.to_celsius(self.regdata2float(raw))
+
+
+if __name__=='__main__':
+  sensor = LM75()
+  print(sensor.getTemp())
