c0426a0129e00d75a090b4f549a18c50b955d577
enrique <enriquesoria@cecotec.es>
2020-05-28 18:17:44 +0200

    Add default value

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ from decouple import AutoConfig
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 config = AutoConfig(f'{BASE_DIR}/')
 
-MQTT_SERVER = config('MQTT_SERVER')
+MQTT_SERVER = config('MQTT_SERVER', default='localhost')
