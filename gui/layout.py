1b703574594d3aafabeb2001834da46e8e277063
EnriqueSoria <ensomag@inf.upv.es>
2020-09-13 16:37:53 +0200

    Make time bigger & justify text

--- gui/layout.py
+++ gui/layout.py
@@ -3,16 +3,16 @@ from gui.enumerations import Inputs, Outputs
 
 def layout(sg):
     font = (sg.DEFAULT_FONT[0], 40)
-    bigger_font = (sg.DEFAULT_FONT[0], 100)
+    bigger_font = (sg.DEFAULT_FONT[0], 200)
 
     DATETIME_ROWS = [
         [sg.Text('----', size=(30, 1), font=bigger_font, justification="center", key=Outputs.NOW)]
     ]
     TEMPERATURE_ROWS = [
-        [sg.Text(size=(30, 1), font=font, key=Outputs.TEMPERATURE)],
+        [sg.Text(size=(30, 1), font=font, justification="center", key=Outputs.TEMPERATURE)],
     ]
     MUSIC_ROWS = [
-        [sg.Text(size=(30, 1), font=font, key=Outputs.CURRENT_SONG)],
+        [sg.Text(size=(30, 1), font=font, justification="center", key=Outputs.CURRENT_SONG)],
         [
             sg.Button('ᐊ', font=font, key=Inputs.PREVIOUS_SONG),
             sg.Button('⬜', font=font, key=Inputs.PLAY_PAUSE_SONG),
@@ -21,6 +21,7 @@ def layout(sg):
     ]
 
     return [
+        [],[],
         *DATETIME_ROWS,
         *MUSIC_ROWS,
         *TEMPERATURE_ROWS,
